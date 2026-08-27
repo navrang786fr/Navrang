abe2a78c476da3e42483c763af43113c7756d083
Navrang <navrang786fr@gmail.com>
2026-08-28 01:14:49 +0300

    Admin: update dish #4 (admin)

--- menu-data.js
+++ menu-data.js
@@ -4,7 +4,7 @@ var MENU_DATA = {
     { id: 1, name: "Egg Burji", nameTe: "గుడ్డు బుర్జీ", price: 110, veg: false, thumb: "images/menu/egg-burji-100.jpg", photo: "images/menu/egg-burji-500.jpg", top: true, ingredients: ["Eggs", "Onion", "Tomato", "Green Chili", "Ginger-Garlic", "Turmeric", "Coriander Leaves"], ingredientsTe: ["గుడ్లు", "ఉల్లిపాయ", "టమాట", "పచ్చిమిర్చి", "అల్లం-వెల్లుల్లి", "పసుపు", "కొత్తిమీర"], status: "Active", weeklyDishInd: false },
     { id: 2, name: "Egg Chilli", nameTe: "గుడ్డు చిల్లీ", price: 130, veg: false, status: "Active", weeklyDishInd: false },
     { id: 3, name: "Egg Curry", nameTe: "గుడ్డు కర్రీ", price: 110, veg: false, thumb: "images/menu/egg-currey-100.jpg", photo: "images/menu/egg-currey-500.jpg", status: "Active", weeklyDishInd: false },
-    { id: 4, name: "Egg Dal", nameTe: "గుడ్డు దాల్", price: 130, veg: false, status: "Active", weeklyDishInd: false },
+    { id: 4, name: "Egg Dal", nameTe: "గుడ్డు దాల్", price: 130, veg: false, thumb: "images/menu/egg-dal-100.jpg", photo: "images/menu/egg-dal-500.jpg", top: false, status: "Active", weeklyDishInd: false },
     { id: 5, name: "Egg Fry", nameTe: "గుడ్డు ఫ్రై", price: 130, veg: false, thumb: "images/menu/egg-fry-100.jpg", photo: "images/menu/egg-fry-500.jpg", top: false, status: "Active", weeklyDishInd: false },
     { id: 6, name: "Egg Keema", nameTe: "గుడ్డు కీమా", price: 110, veg: false, thumb: "images/menu/egg-keema-100.jpg", photo: "images/menu/egg-keema-500.jpg", status: "Active", weeklyDishInd: false },
     { id: 7, name: "Egg Manchuria", nameTe: "గుడ్డు మంచూరియా", price: 140, veg: false, status: "Active", weeklyDishInd: false },
