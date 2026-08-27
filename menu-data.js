9b5082e352a9f0e900b6f7e0f589a5a3a01ee39d
Navrang <navrang786fr@gmail.com>
2026-08-28 00:58:26 +0300

    Admin: update dish #55 (admin)

--- menu-data.js
+++ menu-data.js
@@ -63,7 +63,7 @@ var MENU_DATA = {
   ],
   "biryani": [
     { id: 54, name: "Dum Biryani", nameTe: "దమ్ బిర్యానీ", price: 170, veg: false, top: true, status: "Active", weeklyDishInd: false },
-    { id: 55, name: "Fry Biryani", nameTe: "ఫ్రై బిర్యానీ", price: 180, veg: false, status: "Active", weeklyDishInd: false },
+    { id: 55, name: "Fry Biryani", nameTe: "ఫ్రై బిర్యానీ", price: 180, veg: false, thumb: "images/menu/fry-biryani-100.jpg", photo: "images/menu/fry-biryani-500.jpg", top: false, status: "Active", weeklyDishInd: false },
     { id: 56, name: "Mutton Fry Biryani", nameTe: "మటన్ ఫ్రై బిర్యానీ", price: 360, veg: false, thumb: "images/menu/mutton-fry-biryani-100.jpg", photo: "images/menu/mutton-fry-biryani-500.jpg", top: false, status: "Active", weeklyDishInd: false },
     { id: 57, name: "Natukodi Fry Biryani", nameTe: "నాటుకోడి ఫ్రై బిర్యానీ", price: 310, veg: false, thumb: "images/menu/natukodi-fry-biryani-100.jpg", photo: "images/menu/natukodi-fry-biryani-500.jpg", top: false, status: "Active", weeklyDishInd: false },
     { id: 58, name: "Prawns Fry Biryani", nameTe: "రొయ్యల ఫ్రై బిర్యానీ", price: 320, veg: false, thumb: "images/menu/prawns-fry-biryani-100.jpg", photo: "images/menu/prawns-fry-biryani-500.jpg", top: false, status: "Active", weeklyDishInd: false },
