e0fb1c185dd0b34bdf29a1658d1f9948544c962c
Navrang <navrang786fr@gmail.com>
2026-08-28 01:35:10 +0300

    Admin: update dish #60 (admin)

--- menu-data.js
+++ menu-data.js
@@ -70,7 +70,7 @@ var MENU_DATA = {
     { id: 59, name: "SP Biryani", nameTe: "SP బిర్యానీ", price: 220, veg: false, thumb: "images/menu/sp-biryani-100.jpg", photo: "images/menu/sp-biryani-500.jpg", top: false, status: "Active", weeklyDishInd: false }
   ],
   "roti": [
-    { id: 60, name: "Katak Roti", nameTe: "కటక్ రొట్టె", price: 10, veg: true, status: "Active", weeklyDishInd: false },
+    { id: 60, name: "Katak Roti", nameTe: "కటక్ రొట్టె", price: 10, veg: true, thumb: "images/menu/katak-roti-100.jpg", photo: "images/menu/katak-roti-500.jpg", top: false, status: "Active", weeklyDishInd: false },
     { id: 61, name: "Patala Roti", nameTe: "పాతల రొట్టె", price: 10, veg: true, thumb: "images/menu/patala-roti-100.jpg", photo: "images/menu/patala-roti-500.jpg", top: false, status: "Active", weeklyDishInd: false },
     { id: 62, name: "Ponga Roti", nameTe: "పొంగా రొట్టె", price: 12, veg: true, thumb: "images/menu/ponga-roti-100.jpg", photo: "images/menu/ponga-roti-500.jpg", top: false, status: "Active", weeklyDishInd: false }
   ],
